f05076f90a2873b45a8a6a384c46c4001be869f5
tYTYKVOu <danya.polishchuk.2001@ukr.net>
2020-06-29 15:55:06 +0300

    part1, part2

--- src/main/java/com/epam/rd/java/basic/practice1/Part2.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part2.java
@@ -4,6 +4,12 @@ public class Part2 {
 
     public static void main(String[] args) {
 
+        int sum = 0;
+        for(String arg : args){
+            sum += Integer.parseInt(arg);
+        }
+        System.out.print(sum);
+
     }
 	
 }
