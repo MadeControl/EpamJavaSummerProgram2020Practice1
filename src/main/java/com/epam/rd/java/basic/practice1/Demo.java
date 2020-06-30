426f7400cc9dae08160e6d93d50ef1838da66d6a
tYTYKVOu <danya.polishchuk.2001@ukr.net>
2020-06-30 11:40:35 +0300

    demo changed

--- src/main/java/com/epam/rd/java/basic/practice1/Demo.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Demo.java
@@ -5,25 +5,8 @@ public class Demo {
 
     // Demo method without enters between tasks
     public static void main(String[] args) {
-        String[] args1 = new String[]{"Hello, World"};
-        Part1.main(args1);
 
-        args1 = new String[]{"1","2","3"};
-        Part2.main(args1);
-
-        args1 = new String[]{"A","B","C"};
-        Part3.main(args1);
-
-        args1 = new String[]{"5","10"};
-        Part4.main(args1);
-
-        args1 = new String[]{"1","2","3"};
-        Part5.main(args1);
-
-        args1 = new String[]{"5"};
-        Part6.main(args1);
-
-        args1 = new String[]{"Good bye"};
+        String[] args1 = new String[]{"Good bye"};
         Part7.main(args1);
     }
 
